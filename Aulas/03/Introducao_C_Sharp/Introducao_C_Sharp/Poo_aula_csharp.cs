571facb5c5958da397d3d23160779eafbed91aee
Wenderson Luís dos Santos <wenderson.software.dev@gmail.com>
2025-09-11 16:05:51 -0300

    adicionando aula 3 C Sharp

---
+++ Aulas/03/Introducao_C_Sharp/Introducao_C_Sharp/Poo_aula_csharp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introducao_C_Sharp
+{
+    public class Poo_aula_csharp
+    {
+        static void Main(string[] args)
+        {
+            //Criando variaveis com instâncias da classe Pessoa
+            Pessoa p1 = new Pessoa("Diego", "Inácio", 35);
+            Pessoa p2 = new Pessoa("Maria", "Silva", 20);
+
+            // Exibindo os dados
+            Console.WriteLine($"Pessoa 1: {p1.Nome} - {p1.Sobrenome} - {p1.Idade} anos.");
+            Console.WriteLine($"Pessoa 2: {p2.Nome} - {p2.Sobrenome} - {p1.Idade} anos.");
+
+        }
+    }
+}
---
+++ Aulas/03/Introducao_C_Sharp/Introducao_C_Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Pessoa
+
+{   // Definindo os nomes dos atributos
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+
+    public int Idade { get; set; }
+
+    public Pessoa(string nome, string sobrenome, int idade)
+
+    // Construtor
+
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+        Idade = idade;
+    }
+
+}

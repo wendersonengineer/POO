f6d8f447bb1b19ef81e817d2f6ce57e8fb4e9673
Wenderson Luís dos Santos <wenderson.software.dev@gmail.com>
2025-09-14 10:00:54 -0300

    Atividade 4 de POO classe carro/classe chave / python

--- Aulas/04/Car.py
+++ Aulas/04/Car.py
@@ -1,10 +1,18 @@
+import time
+
 class Chave:
     def __init__(self, marca):
-        # Método construtor
         self.marca = marca 
         self.ativa = False
 
-# Define a Classe
+    def GirarChave(self):
+        if not self.ativa:
+            self.ativa = True
+            print("A chave foi ativada!")
+        else:
+            print("A chave já foi ativada!")
+
+
 class Carro:
     def __init__(self, modelo, ano, cor, potencia, placa, chave: Chave):
         self.modelo = modelo
@@ -12,38 +20,67 @@ class Carro:
         self.cor = cor
         self.potencia = potencia
         self.placa = placa
-        self.ligado = False # não precisa ser definido por ser False
-        self.aberto = False # não precisa ser definido por ser False
+        self.ligado = False 
+        self.aberto = False 
         self.chave = chave
         self.velocidade = 0
 
-    # Métodos  | Ação realizada pela classe
     def AbrirCarro(self, chave):
-        if not self.ligado and not self.aberto and self.chave == chave.marca:
-            self.aberto = True    # Self.aberto recebe True
+        if not self.ligado and not self.aberto and self.chave == chave:
+            self.aberto = True    
             print(f"O carro esta aberto!")
+            self.chave.GirarChave()
         else:
             print(f"O carro já esta aberto ou ligado ou a chave esta errada!")
 
-    # Método ligar o carro
     def LigarCarro(self):
-        if not self.ligado and self.aberto:
+        if self.aberto and not self.ligado and self.chave.ativa:
             self.ligado = True
-            print("O carro esta ligado RUMMMMM!")
+            print("O carro esta ligado!")
+        elif not self.aberto:
+            print("Abra o carro antes de ligar")
+        elif not self.chave.ativa:
+            print("A chave não foi girada, precisa ativar a chave!")
         else:
-            print("O carro esta ligado ou aberto, verifique isso!")
+            print("O carro esta ligado, rummmmm!")
 
-    # Método Acelerar o carro
     def AcelerarCarro(self):
-        if self.ligado and self.velocidade >= 0:
+        if self.ligado:
             self.velocidade += 5
-            print(f"O carro {self.modelo} velocidade {self.velocidade} km.")
+            print(f"O carro {self.modelo} está acelerando - Velocidade {self.velocidade} km/h.")
         else:
-            print(f"O carro {self.modelo} esta desligado, precisa ligar primeiro!!!")
+            print(f"O carro {self.modelo} está desligado - não é possível acelerar no momento!")
+
+
+    def FrearCarro(self):
+        if self.ligado and self.velocidade > 0:
+            self.velocidade -= 5
+            if self.velocidade < 0:
+                self.velocidade = 0
+            print(f"O carro {self.modelo} está freando - Velocidade: {self.velocidade} Km/h")
+        else:
+            print(f"O carro {self.modelo} está parado ou desligado!")
+
+    def FrearTotalmente(self):
+        if self.ligado and self.velocidade > 0:
+            print(f"O Freio foi acionado totalmente no carro {self.modelo}!")
+            while self.velocidade > 0:
+                self.FrearCarro()    
+                time.sleep(0.5)
+            print(f"O carro {self.modelo} parou totamente.")
+        else:
+            print(f"O carro {self.modelo} já está parado ou desligado!")
+
+
+    def DesligarCarro(self):
+        if not self.ligado:
+            print("O carro já está desligado!")
+        elif self.velocidade > 0:
+            print("Não foi possivel desligar o carro em movimento!")
+        else:
+            self.ligado = False
+            print("O carro foi desligado!")
+        
+    
             
  
-
-#ch = Chave("FIAT")
-#car1 = Car("Uno", 1999, "Verde", 1.0, "KPJ-0077", ch)
-#car1.AbrirCarro(ch)
-#print(car1.chave.marca)
--- Aulas/04/main.py
+++ Aulas/04/main.py
@@ -1,27 +1,24 @@
 from Car import Chave
 from Car import Carro
 
+
 ch = Chave("FIAT")
-ch1 = Chave("FERRARI")
 
-car1 = Carro("Uno", 1999, "Verde", 1.0, "KPJ-0077", ch)
+car1 = Carro("Uno", 1999, "Verde", 1.0, "SQ007", ch)
 
-# Vai da erro
-car1.LigarCarro()
-
-# Vai da certo
 car1.AbrirCarro(ch)
-
-# Vai da certo
 car1.LigarCarro()
 
-# Acelerar o carro
-for i in range(5):
+for i in range(6):
     car1.AcelerarCarro()
 
-# Print --->
-print(car1.chave.marca)
+car1.DesligarCarro()
 
+car1.FrearTotalmente()
+car1.DesligarCarro()
+
+#Atividade POO:
 # O carro tem que desligar, mas náo pode desligar acelerado
-# Tem que ter um méetodo de freiar de 5 em 5
+# Tem que ter um método de freiar de 5 em 5
 # e tem que ter a inserção da ativação da chave assim que abrir o for True.
+
